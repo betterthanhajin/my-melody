0e856d71963577612325b699811a5633d69229ee
Hajin Lee <betterthanhajin@icloud.com>
2024-10-14 23:55:41 +0900

    feat: 버튼보더인터렉션추가

--- components/youtubeMusic/MusicMy.tsx
+++ components/youtubeMusic/MusicMy.tsx
@@ -1,7 +1,22 @@
+"use client";
 import Image from "next/image";
 import test from "@/public/images/test1.png";
+import { use, useRef, useEffect } from "react";
+import { keyframes } from "@emotion/react";
+import { border, color } from "@mui/system";
 
 export default function MusicMy() {
+  const borderRef = useRef<HTMLDivElement>(null);
+  const colors = ["pink", "purple", "blue", "green", "yellow", "red"];
+
+  useEffect(() => {
+    if (borderRef.current) {
+      borderRef.current.style.border = `1px solid ${
+        colors[Math.floor(Math.random() * colors.length)]
+      }`;
+    }
+  }, []);
+
   return (
     <section className="w-full h-[52px] flex justify-between gap-8">
       <div className="flex gap-4">
@@ -15,12 +30,17 @@ export default function MusicMy() {
           />
         </div>
         <div>
-          <h3 className="font-extralight">START RADIO FROM A SONG</h3>
+          <h3 className="font-extralight text-xs whitespace-nowrap">
+            START RADIO FROM A SONG
+          </h3>
           <p className="font-extrabold">Quick picks</p>
         </div>
       </div>
-      <div>
-        <span className="whitespace-nowrap">Play all</span>
+      <div
+        className="w-[100px] h-[42px] text-center rounded-2xl border-[1px] from-pink-300 to-purple-500 border-transparent p-2"
+        ref={borderRef}
+      >
+        <span className="whitespace-nowrap text-xs">Play all</span>
       </div>
     </section>
   );
