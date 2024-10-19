b17d3f9ca375304c35af6147ccce4e3e30e58384
Hajin Lee <betterthanhajin@icloud.com>
2024-10-20 02:14:03 +0900

    fix: debug

--- components/youtubeMusic/MusicHeader.tsx
+++ components/youtubeMusic/MusicHeader.tsx
@@ -6,13 +6,9 @@ import MusicMy from "./MusicMy";
 export default function MusicHeader() {
   return (
     <header className="w-full h-[20px] flex justify-between items-center mb-12">
-      <div className="flex gap-4">
+      <div className="flex items-center gap-2">
         <Image src={youtubeLogo} alt="youtube" width={65} height={65} />
-      </div>
-      <div className="flex gap-4">
-        <button className="font-bold">Home</button>
-        <button className="font-bold">Explore</button>
-        <button className="font-bold">Library</button>
+        <div className="font-bold text-2xl">Music</div>
       </div>
       <div className="flex gap-4">
         <Image src={youtubeSearch} alt="youtube" width={35} height={35} />
